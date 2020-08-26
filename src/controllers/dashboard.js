4b45fcc4e6752b3f07b065a2e4bf6a199ee74056
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2020-08-27 00:08:13 +0700

    finish backend

--- src/controllers/dashboard.js
+++ src/controllers/dashboard.js
@@ -31,12 +31,24 @@ module.exports = {
 
       const total_trip = await detailtrip.findAll();
 
+      const trip_to_sales_done = await checkout.findAll({
+        include: [
+          {
+            model: sales,
+          },
+          {
+            model: apotik,
+          },
+        ],
+      });
+
       res.send({
         message: true,
         totalProduct: totalProduct.length,
         totalProductBuy: orderTotal.length,
         totalSales: totalSales.length,
         totalTrip: total_trip.length,
+        trip_to_sales_done,
       });
     } catch (error) {
       console.log(error);
